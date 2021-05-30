4130e190868fd4c4d0f6a7c4cea249a646921798
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-05-30 20:12:20 +0900

    zx を試す

---
+++ zx/README.md
@@ -0,0 +1,1 @@
+https://github.com/google/zx を試す。
---
+++ zx/helloWorld.mjs
@@ -0,0 +1,20 @@
+let count = parseInt(await $`ls -1 | wc -l`)
+console.log(`Files count: ${count}`)
+
+await $`docker --version`
+await $`aws --version`
+
+const displayWord = (word) => {
+  console.log(`受け取った word: ${word}`);
+}
+
+displayWord("hogehoge");
+
+const getRandomWord = () => {
+  return Math.random().toString(32).substring(2);
+}
+
+await $`echo ${getRandomWord()}`
+
+const string = await $`cat README.md`
+console.log(`READMEの中身:\n ${string}`)
