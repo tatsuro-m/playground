78dbc277d2fc3198251eb6ce6e6027767e375d6e
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-06-08 20:51:54 +0900

    rollback main func

--- prisma/start_from_scratch/index.ts
+++ prisma/start_from_scratch/index.ts
@@ -3,11 +3,27 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function main() {
-  const post = await prisma.post.update({
-    where: {id: 1},
-    data: {published: true},
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: {title: 'Hello World'},
+      },
+      profile: {
+        create: {bio: 'I like turtles'},
+      },
+    },
   })
-  console.log(post)
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true
+    },
+  })
+
+  console.dir(allUsers, {depth: null})
 }
 
 main()
