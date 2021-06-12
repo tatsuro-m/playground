fe352f0fc82dbac9559988f58c2b06e6fa445e2e
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-12 10:14:54 +0900

    Revert "wip"
    This reverts commit ab400ff7

--- go/cognito-jwt-verify/main.go
+++ go/cognito-jwt-verify/main.go
@@ -47,16 +47,12 @@ func main() {
 		}
 
 		if _, ok := targetKey["kid"]; !ok {
-			return "", fmt.Errorf("key with specified kid is not present in jwks")
+			return nil, fmt.Errorf("key with specified kid is not present in jwks")
 		}
 
 		fmt.Printf("cognito が公開している鍵で使うべき方: %v\n", targetKey)
 		return "", nil
 	})
 
-	if err != nil {
-		fmt.Printf("エラーがあったっぽい: %v", err)
-		return
-	}
-	fmt.Printf("最終的な Claims: %v\n", token.Claims)
+	fmt.Printf("最終的な token: %v\n", token)
 }
