d45a7bbfe5b6aec1e994b8fbc36fdb1834e6d3f1
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-16 20:14:06 +0900

    add field

--- go/gin-gorm-tutorial/entity/post.go
+++ go/gin-gorm-tutorial/entity/post.go
@@ -3,9 +3,10 @@ package entity
 import "time"
 
 type Post struct {
-	ID        uint      `json:"id" gorm:"autoIncrement;primaryKey"`
-	Title     string    `json:"title" binding:"required" gorm:"not null"`
-	Content   string    `json:"content" binding:"required" gorm:"not null"`
+	ID        uint   `json:"id" gorm:"autoIncrement;primaryKey"`
+	Title     string `json:"title" binding:"required" gorm:"not null"`
+	Content   string `json:"content" binding:"required" gorm:"not null"`
+	User      User
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
