e11a7817c272e33202dd33ac37358cdc8cbeaaf1
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-19 08:59:22 +0900

    add crud interface

--- go/gin-gorm-tutorial/service/post/post.go
+++ go/gin-gorm-tutorial/service/post/post.go
@@ -3,6 +3,8 @@ package post
 import (
 	"gin-gorm-tutorial/db"
 	"gin-gorm-tutorial/entity"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Service struct{}
@@ -14,6 +16,60 @@ func (s Service) GetAll() ([]Post, error) {
 
 	if err := d.Find(&p).Error; err != nil {
 		return nil, err
+	}
+
+	return p, nil
+}
+
+func (s Service) GetByID(id string) (Post, error) {
+	d := db.GetDB()
+	var p Post
+
+	if err := d.Where("id = ?", id).First(&p).Error; err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
+
+func (s Service) CreateModel(c *gin.Context) (Post, error) {
+	d := db.GetDB()
+	var p Post
+
+	if err := c.BindJSON(&p); err != nil {
+		return p, err
+	}
+
+	if err := d.Create(&p).Error; err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
+
+func (s Service) UpdateByID(id string, c *gin.Context) (Post, error) {
+	d := db.GetDB()
+	var p Post
+
+	if err := d.Where("id = ?", id).First(&p).Error; err != nil {
+		return p, err
+	}
+
+	if err := c.BindJSON(&p); err != nil {
+		return p, err
+	}
+
+	d.Save(&p)
+	return p, nil
+}
+
+func (s Service) DeleteByID(id string) (Post, error) {
+	d := db.GetDB()
+	var p Post
+
+	data := d.Where("id = ?", id).First(&p)
+	if err := data.Delete(&p).Error; err != nil {
+		return p, err
 	}
 
 	return p, nil
