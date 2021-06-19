e3275dfb82d8ebd6cb3c3eaa523622610f315686
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-19 09:28:11 +0900

    add method

--- go/gin-gorm-tutorial/controller/post/post.go
+++ go/gin-gorm-tutorial/controller/post/post.go
@@ -48,3 +48,31 @@ func (ct Controller) Show(c *gin.Context) {
 		c.JSON(http.StatusOK, p)
 	}
 }
+
+// PUT /posts/:id
+func (ct Controller) Update(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s post.Service
+
+	p, err := s.UpdateByID(id, c)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		fmt.Println(err)
+	} else {
+		c.JSON(http.StatusOK, p)
+	}
+}
+
+// DELETE /posts/:id
+func (ct Controller) Delete(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s post.Service
+
+	p, err := s.DeleteByID(id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		fmt.Println(err)
+	} else {
+		c.JSON(http.StatusOK, p)
+	}
+}
