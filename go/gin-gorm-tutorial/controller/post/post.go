7e37f8d236276e50a2c53da8a0eaa7582e2cfc6e
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-19 09:21:03 +0900

    add method

--- go/gin-gorm-tutorial/controller/post/post.go
+++ go/gin-gorm-tutorial/controller/post/post.go
@@ -22,3 +22,29 @@ func (ct Controller) Index(c *gin.Context) {
 		c.JSON(http.StatusOK, p)
 	}
 }
+
+// POST /posts
+func (ct Controller) Create(c *gin.Context) {
+	var s post.Service
+
+	if p, err := s.CreateModel(c); err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		fmt.Println(err)
+	} else {
+		c.JSON(http.StatusCreated, p)
+	}
+}
+
+// GET /posts/:id
+func (ct Controller) Show(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s post.Service
+
+	p, err := s.GetByID(id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		fmt.Println(err)
+	} else {
+		c.JSON(http.StatusOK, p)
+	}
+}
