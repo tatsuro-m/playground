143ca3af93cf72fed6f686ba342c00a05371a5fb
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-27 11:34:13 +0900

    add cloud sql

--- go/gin-gorm-tutorial/infrastructure/main.tf
+++ go/gin-gorm-tutorial/infrastructure/main.tf
@@ -6,24 +6,57 @@ terraform {
     }
   }
   backend "gcs" {
-    bucket  = "playground-tfstate-949181"
-    prefix  = "gin-gorm-tutorial/dev"
+    bucket = "playground-tfstate-949181"
+    prefix = "gin-gorm-tutorial/dev"
   }
 }
 
 provider "google" {
-  region  = "asia-northeast1"
+  region = "asia-northeast1"
 }
 
 variable "billing_account_id" {}
 
 resource "google_project" "my_project" {
-  name       = "gin-gorm-tutorial"
-  project_id = "gin-gorm-tutorial-2348792381"
+  name            = "gin-gorm-tutorial"
+  project_id      = "gin-gorm-tutorial-2348792381"
   billing_account = var.billing_account_id
 }
 
 resource "google_compute_network" "vpc_network" {
   project = google_project.my_project.project_id
-  name = "terraform-network"
+  name    = "terraform-network"
 }
+
+resource "google_sql_database_instance" "master" {
+  project          = google_project.my_project.project_id
+  name             = "master-instance"
+  database_version = "POSTGRES_13"
+
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+      authorized_networks {
+        name  = "all network access"
+        value = "0.0.0.0/0"
+      }
+    }
+  }
+}
+
+variable "POSTGRES_DB_NAME" {}
+resource "google_sql_database" "database" {
+  project  = google_project.my_project.project_id
+  name     = var.POSTGRES_DB_NAME
+  instance = google_sql_database_instance.master.name
+}
+
+variable "POSTGRES_PASSWORD" {}
+resource "google_sql_user" "users" {
+  project  = google_project.my_project.project_id
+  name     = "postgres"
+  instance = google_sql_database_instance.master.name
+  password = var.POSTGRES_PASSWORD
+}
+
