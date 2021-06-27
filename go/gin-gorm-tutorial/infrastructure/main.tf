b245904c7ca7eca28d427da9da31426aa2e134b6
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-06-27 10:18:32 +0900

    setup account

--- go/gin-gorm-tutorial/infrastructure/main.tf
+++ go/gin-gorm-tutorial/infrastructure/main.tf
@@ -12,10 +12,18 @@ terraform {
 }
 
 provider "google" {
-  project = "playground-318023"
   region  = "asia-northeast1"
 }
 
+variable "billing_account_id" {}
+
+resource "google_project" "my_project" {
+  name       = "gin-gorm-tutorial"
+  project_id = "gin-gorm-tutorial-2348792381"
+  billing_account = var.billing_account_id
+}
+
 resource "google_compute_network" "vpc_network" {
+  project = google_project.my_project.project_id
   name = "terraform-network"
 }
