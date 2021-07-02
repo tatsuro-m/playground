efd59533c842560fc5679af93e805c0f0eec3b15
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-02 12:51:24 +0900

    add desc

--- go/algorithm/sort/bubble/main.go
+++ go/algorithm/sort/bubble/main.go
@@ -8,18 +8,36 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	s := make([]int, 1000)
+	s := make([]int, 100)
 	for i := range s {
 		s[i] = rand.Intn(100)
 	}
 
-	fmt.Println(exec(s))
+	fmt.Println(asc(s))
+	fmt.Println(desc(s))
 }
 
-func exec(s []int) []int {
+func asc(s []int) []int {
 	for i := 0; i < len(s); i++ {
 		for j := i + 1; j < len(s); j++ {
+			// 先頭と２つ目（i + 1 だから）を比較して、１つ目の方が大きいかをチェック
+			//　昇順したいので、大きい要素は小さい要素よりも後ろに来なくてはいけない
 			if s[i] > s[j] {
+				// 単純に前後を入れ替えているだけ
+				// slice に対してインデックス番号を指定して書き込んでいるので slice 自体の順番が整っていく
+				s[i], s[j] = s[j], s[i]
+			}
+		}
+		// 以降 i をインクリメントしながらスライスの長さ分だけ繰り返すので、比較する要素が（１個目と２個目、２個目と３個目 ...）のように変わっていく
+	}
+
+	return s
+}
+
+func desc(s []int) []int {
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] < s[j] {
 				s[i], s[j] = s[j], s[i]
 			}
 		}
--- go/algorithm/sort/bubble/main_test.go
+++ go/algorithm/sort/bubble/main_test.go
@@ -8,18 +8,38 @@ import (
 	"time"
 )
 
-func TestExec(t *testing.T) {
+func TestAsc(t *testing.T) {
 	t.Run("昇順にソートされていること", func(t *testing.T) {
 		rand.Seed(time.Now().Unix())
-		s := make([]int, 1000)
+		s := make([]int, 100)
 		for i := range s {
 			s[i] = rand.Intn(100)
 		}
 
+		want := asc(s)
 		sort.Ints(s)
-		want := exec(s)
 
 		if !reflect.DeepEqual(s, want) {
+			t.Errorf("結果が違います。")
+		}
+	})
+}
+
+func TestDesc(t *testing.T) {
+	t.Run("降順にソートされていること", func(t *testing.T) {
+		rand.Seed(time.Now().Unix())
+		s := make([]int, 100)
+		for i := range s {
+			s[i] = rand.Intn(100)
+		}
+
+		c := make([]int, 100)
+		copy(c, s)
+
+		sort.Sort(sort.Reverse(sort.IntSlice(c)))
+		s = desc(s)
+
+		if !reflect.DeepEqual(c, s) {
 			t.Errorf("結果が違います。")
 		}
 	})
