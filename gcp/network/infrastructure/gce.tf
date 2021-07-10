5f6159f10937bb3c55f10e36e075439b8547da37
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-10 15:03:09 +0900

    add service account

--- gcp/network/infrastructure/gce.tf
+++ gcp/network/infrastructure/gce.tf
@@ -1,7 +1,14 @@
+resource "google_service_account" "my_service_account" {
+  project = google_project.my_project.project_id
+  account_id   = "my-service-account"
+  display_name = "My Service Account"
+}
+
 resource "google_compute_instance" "default" {
   project      = google_project.my_project.project_id
   name         = "test"
   machine_type = "e2-micro"
+  allow_stopping_for_update = true
 
   tags = [
     "foo",
@@ -20,4 +27,8 @@ resource "google_compute_instance" "default" {
     }
   }
 
+  service_account {
+    email  = google_service_account.my_service_account.email
+    scopes = ["cloud-platform"]
+  }
 }
