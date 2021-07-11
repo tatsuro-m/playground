56041aa7b361aa49b4afb237c46723339d99cab4
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-11 15:59:04 +0900

    add bucket

--- gcp/ci-cd/infrastructure/staging/gcs.tf
+++ gcp/ci-cd/infrastructure/staging/gcs.tf
@@ -6,3 +6,12 @@ resource "google_storage_bucket" "a" {
 
   uniform_bucket_level_access = true
 }
+
+resource "google_storage_bucket" "b" {
+  project = google_project.my_project.project_id
+  name          = "test-bucket-${random_string.resource_name_suffix.result}-2"
+  location      = "ASIA-NORTHEAST1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
