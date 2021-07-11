d4af3d9b143cc8bba58366eab7c810f972896e69
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-11 20:10:50 +0900

    test validate

--- gcp/ci-cd/infrastructure/staging/gcs.tf
+++ gcp/ci-cd/infrastructure/staging/gcs.tf
@@ -18,6 +18,7 @@ resource "google_storage_bucket" "c" {
   name          = "${local.project_name}-test-bucket-c"
   location      = "ASIA-NORTHEAST1"
   force_destroy = true
+  hoge = true
 
   uniform_bucket_level_access = true
 }
