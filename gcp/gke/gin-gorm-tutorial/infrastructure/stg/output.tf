6bec6a2208a35496af08c87768883e9938aa3e8c
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-31 11:58:50 +0900

    add output

--- gcp/gke/gin-gorm-tutorial/infrastructure/stg/output.tf
+++ gcp/gke/gin-gorm-tutorial/infrastructure/stg/output.tf
@@ -2,10 +2,22 @@ output "app_prefix" {
   value = local.app_prefix
 }
 
-output "repo-url" {
+output "image_repo_url" {
   value = "${google_artifact_registry_repository.main.location}-docker.pkg.dev/${google_artifact_registry_repository.main.project}/${google_artifact_registry_repository.main.repository_id}"
 }
 
 output "cluster_name" {
   value = google_container_cluster.primary.name
+}
+
+output "db_public_ip" {
+  value = google_sql_database_instance.master.ip_address.0.ip_address
+}
+
+output "db_connection_name" {
+  value = google_sql_database_instance.master.connection_name
+}
+
+output "db_name" {
+  value = google_sql_database.database.name
 }
