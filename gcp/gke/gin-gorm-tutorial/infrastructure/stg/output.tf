d4b1e56a8e10e3a2d31ab55a6dd4a59ee8f110f3
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-31 11:06:07 +0900

    create deployment

--- gcp/gke/gin-gorm-tutorial/infrastructure/stg/output.tf
+++ gcp/gke/gin-gorm-tutorial/infrastructure/stg/output.tf
@@ -2,8 +2,8 @@ output "app_prefix" {
   value = local.app_prefix
 }
 
-output "repo-id" {
-  value = google_artifact_registry_repository.main.id
+output "repo-url" {
+  value = "${google_artifact_registry_repository.main.location}-docker.pkg.dev/${google_artifact_registry_repository.main.project}/${google_artifact_registry_repository.main.repository_id}"
 }
 
 output "cluster_name" {
