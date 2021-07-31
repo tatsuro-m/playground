35b9f2fb4893b59fce7d693e20e93e053c2f27d9
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-07-31 09:11:38 +0900

    fmt

--- gcp/gke/gin-gorm-tutorial/infrastructure/stg/artifact-registry.tf
+++ gcp/gke/gin-gorm-tutorial/infrastructure/stg/artifact-registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "main" {
   provider = google-beta
-  project = "playground-318023"
+  project  = "playground-318023"
 
   location      = "asia-northeast1"
   repository_id = "${local.app_prefix}-main"
