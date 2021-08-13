0c6c800b4325cd949d7c3cc0a875eef1502fe128
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-08-13 14:14:59 +0900

    complete step2

--- graphql/apollo-full-stack-tutorial/fullstack-tutorial-master/start/server/src/index.js
+++ graphql/apollo-full-stack-tutorial/fullstack-tutorial-master/start/server/src/index.js
@@ -1,13 +1,28 @@
-require('dotenv').config();
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({typeDefs});
+const { createStore } = require('./utils');
 
-server.listen().then(() =>{
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(() => {
   console.log(`
-  Server is running!
-  Listening on port 4000
-  Explore at https://studio.apollographql.com/sandbox
-  `)
-})
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
