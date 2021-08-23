f6e5eafe5604709e6ccc5780ba674b60443c3222
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-08-23 10:28:24 +0900

    update container ver

--- gcp/cloud-run/tf-sample/infrastructure/cloud-run.tf
+++ gcp/cloud-run/tf-sample/infrastructure/cloud-run.tf
@@ -5,7 +5,7 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        image = "asia-northeast1-docker.pkg.dev/playground-318023/stg-cloud-run-main/golang-api:v2"
       }
     }
   }
