e6030932d0cc6a785b758ab988251d09961b49fa
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-08-31 17:20:52 +0900

    add test

--- go/sqlboiler-tutorial/service/user/user_test.go
+++ go/sqlboiler-tutorial/service/user/user_test.go
@@ -1,6 +1,10 @@
 package user
 
 import (
+	"context"
+	"math"
+	"sqlboiler-tutorial/db"
+	"sqlboiler-tutorial/models"
 	"sqlboiler-tutorial/thelper"
 	"testing"
 )
@@ -9,4 +13,19 @@ func TestGetAllUsers(t *testing.T) {
 	thelper.SetupTest(t)
 	defer thelper.FinalizeTest(t)
 
+	recordNum := 5
+	thelper.InsertUser(t, recordNum)
+	actual, _ := models.Users().Count(context.Background(), db.GetDB())
+
+	if recordNum != Int64ToInt(actual) {
+		t.Error("error!")
+	}
+}
+
+func Int64ToInt(i int64) int {
+	if i < math.MinInt32 || i > math.MaxInt32 {
+		return 0
+	} else {
+		return int(i)
+	}
 }
