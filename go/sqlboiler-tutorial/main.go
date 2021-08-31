299f0da5df86a4ce31c0ce13bdba56cc56c23fe3
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-08-31 10:15:20 +0900

    mod main file

--- go/sqlboiler-tutorial/main.go
+++ go/sqlboiler-tutorial/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"sqlboiler-tutorial/db"
-	"sqlboiler-tutorial/models"
 )
 
 func main() {
 	fmt.Println("Hello World!")
-	fmt.Println(models.UserColumns.ID)
-	db.Init()
+	fmt.Println("sqlboiler の動作確認のためには別の go ファイルを run してください！")
 }
