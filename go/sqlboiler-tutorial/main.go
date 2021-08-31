7ebe3320d8e3e974a0c0bc22fcdc3b0b0ceca641
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-08-31 09:59:35 +0900

    print const

--- go/sqlboiler-tutorial/main.go
+++ go/sqlboiler-tutorial/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"sqlboiler-tutorial/db"
+	"sqlboiler-tutorial/models"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+	fmt.Println(models.UserColumns.ID)
 	db.Init()
 }
