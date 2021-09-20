0c8020d29d4b4c44577a8955a33eda46c816c36d
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-09-20 11:30:31 +0900

    add users link

--- nextjs/review/pages/index.tsx
+++ nextjs/review/pages/index.tsx
@@ -1,13 +1,24 @@
-import { Button } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 import Link from 'next/link'
 
 export const Home = (): JSX.Element => {
   return (
     <>
-      <h1>next sample!</h1>
-      <Link href="/hello">
-        <Button variant="contained">このボタンを押してね！</Button>
-      </Link>
+      <h1>Next sample!</h1>
+
+      <Grid container justifyItems="center">
+        <Grid item xs={4}>
+          <Link href="/hello">
+            <Button variant="contained">ハロー画面</Button>
+          </Link>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Link href="/users">
+            <Button variant="contained">ユーザ一覧</Button>
+          </Link>
+        </Grid>
+      </Grid>
     </>
   )
 }
