8185620a1ec3f30110c653f4357e34e462d84104
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-09-20 11:18:38 +0900

    install pkg

--- nextjs/review/pages/index.tsx
+++ nextjs/review/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@mui/material'
+
 export const Home = (): JSX.Element => {
-  return <h1>next sample!</h1>
+  return (
+    <>
+      <h1>next sample!</h1>
+      <Button variant="contained">このボタンを押してね！</Button>
+    </>
+  )
 }
 
 export default Home
