43ace57119c58738884d180c8a3800160b0cc72e
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-09-25 17:44:24 +0900

    modify docker env

--- graphql/gqlgen-sample/graphql/graph/schema.resolvers.go
+++ graphql/gqlgen-sample/graphql/graph/schema.resolvers.go
@@ -8,26 +8,10 @@ import (
 	"fmt"
 	"graphql/graph/generated"
 	"graphql/graph/model"
-	"graphql/internal/url"
-	"io/ioutil"
-	"net/http"
 )
 
 func (r *queryResolver) Posts(ctx context.Context) ([]*model.Post, error) {
-	req, err := http.NewRequest(http.MethodGet, url.GetAPIPath("/posts"), nil)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	client := new(http.Client)
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println("手に入れた body")
-	fmt.Println(body)
-
+	fmt.Println("posts query が呼ばれた")
 	return nil, nil
 }
 
