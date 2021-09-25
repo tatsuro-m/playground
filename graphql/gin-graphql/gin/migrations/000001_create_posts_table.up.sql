d36c761b1c8566dac7afeae936495f0532d07a9f
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-09-25 13:25:30 +0900

    change ddl

--- graphql/gin-graphql/gin/migrations/000001_create_posts_table.up.sql
+++ graphql/gin-graphql/gin/migrations/000001_create_posts_table.up.sql
@@ -1,9 +1,7 @@
-CREATE TABLE IF NOT EXISTS articles
+CREATE TABLE posts
 (
     id         int AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    author     varchar(50)        NOT NULL DEFAULT '',
     title      varchar(250)       NOT NULL DEFAULT '',
-    content    varchar(400)       NOT NULL DEFAULT '',
     created_at DATETIME           NOT NULL DEFAULT now(),
     updated_at DATETIME           NOT NULL DEFAULT now()
 );
