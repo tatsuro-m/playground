9334214345884cd598c1d545e20e3b82553e2113
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-09-26 15:23:26 +0900

    setup mui

---
+++ graphql/gqlgen-sample/frontend/pages/_app.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from '../src/theme'
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    jssStyles?.parentElement?.removeChild(jssStyles)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Password Generator</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
--- graphql/gqlgen-sample/frontend/pages/index.tsx
+++ graphql/gqlgen-sample/frontend/pages/index.tsx
@@ -2,6 +2,16 @@ import client from '../apollo-client'
 import { gql } from '@apollo/client'
 import { GetStaticProps } from 'next'
 import React from 'react'
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core'
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await client.query({
@@ -37,13 +47,30 @@ interface Props {
 
 export const Home: React.VFC<Props> = (props) => {
   return (
-    <>
-      <h1>Posts</h1>
-      <h1>{props.posts[0].title}</h1>
-      <h1>{props.posts[1].id}</h1>
-      <h1>{props.posts[1].createdAt}</h1>
-      <h1>{props.posts[1].updatedAt}</h1>
-    </>
+    <Box m={10}>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>タイトル</TableCell>
+              <TableCell align="right">作成日時</TableCell>
+              <TableCell align="right">更新日時</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.posts.map((post) => (
+              <TableRow key={post.id}>
+                <TableCell component="th" scope="row">
+                  {post.title}
+                </TableCell>
+                <TableCell align="right">{post.createdAt}</TableCell>
+                <TableCell align="right">{post.updatedAt}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
 
