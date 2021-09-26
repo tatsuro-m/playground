387a48ace3c562625e9da3aec72ab190948dd7fe
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-09-26 17:51:53 +0900

    add redirect logic

--- graphql/gqlgen-sample/frontend/pages/sign_in.tsx
+++ graphql/gqlgen-sample/frontend/pages/sign_in.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import { auth, uiConfig } from '../src/lib/firebase'
+import { useRouter } from 'next/router'
 
 const sign_in: React.VFC = () => {
+  const router = useRouter()
+  if (auth.currentUser) {
+    router.push('/')
+  }
+
   return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
 }
 
