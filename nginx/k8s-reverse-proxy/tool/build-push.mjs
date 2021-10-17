24ff6b1eba16125ef732a40d3518fd78ce085101
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-10-17 12:14:51 +0900

    create namespace

--- nginx/k8s-reverse-proxy/tool/build-push.mjs
+++ nginx/k8s-reverse-proxy/tool/build-push.mjs
@@ -4,9 +4,12 @@ await $`echo Dockerイメージをビルドして対応するレジストリに 
 
 const imageTag = Math.floor(new Date().getTime() / 1000)
 
+// common
+await $`kubectl apply -f ../infrastructure/kubernets/common -R`
+
 // nginx
 let appName = 'nginx'
 let registryURI = 'asia-northeast1-docker.pkg.dev/playground-318023/stg-rproxy-nginx-proxy'
 await $`cd ../${appName} && docker build -t ${registryURI}/${appName}:${imageTag} . && docker push ${registryURI}/${appName}:${imageTag}`
 await $`echo イメージタグを更新します`
-await $`cd ../infrastructure/kubernets/nginx/overlays/stg && kustomize edit set image ${registryURI}/${appName}:${imageTag}`
+await $`cd ../infrastructure/kubernets/${appName}/overlays/stg && kustomize edit set image ${registryURI}/${appName}:${imageTag}`
