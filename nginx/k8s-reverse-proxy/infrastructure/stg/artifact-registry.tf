d71f32a99d04dbd50f31a4eb3d449b8d98d4226b
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-10-17 12:59:43 +0900

    setup frontend

--- nginx/k8s-reverse-proxy/infrastructure/stg/artifact-registry.tf
+++ nginx/k8s-reverse-proxy/infrastructure/stg/artifact-registry.tf
@@ -1,9 +1,19 @@
-resource "google_artifact_registry_repository" "main" {
+resource "google_artifact_registry_repository" "nginx" {
   provider = google-beta
   project  = "playground-318023"
 
   location      = var.default_region
   repository_id = "${local.app_prefix}-nginx-proxy"
   description   = "nginx reverse proxy repository"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "frontend" {
+  provider = google-beta
+  project  = "playground-318023"
+
+  location      = var.default_region
+  repository_id = "${local.app_prefix}-frontend"
+  description   = "web front end"
   format        = "DOCKER"
 }
--- nginx/k8s-reverse-proxy/tool/update-k8s.mjs
+++ nginx/k8s-reverse-proxy/tool/update-k8s.mjs
@@ -18,3 +18,7 @@ await $`echo イメージタグを更新します`
 await $`cd ../infrastructure/kubernets/${appName}/overlays/stg && kustomize edit set image ${registryURI}/${appName}:${imageTag}`
 await $`kubectl apply -k ../infrastructure/kubernets/${appName}/overlays/stg`
 
+// frontend
+appName = 'frontend'
+registryURI = 'asia-northeast1-docker.pkg.dev/playground-318023/stg-rproxy-frontend'
+await $`cd ../${appName} && docker build --platform amd64 -t ${registryURI}/${appName}:${imageTag} . && docker push ${registryURI}/${appName}:${imageTag}`
