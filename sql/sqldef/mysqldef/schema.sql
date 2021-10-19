b93b4f2c7da5652fdf159aafd8346cc08b36624b
tatsuro-m <chrome0130@gmail.com>
2021-10-19 12:48:43 +0900

    add table

--- sql/sqldef/mysqldef/schema.sql
+++ sql/sqldef/mysqldef/schema.sql
@@ -1,10 +1,17 @@
 CREATE TABLE `user`
 (
-    `id`   bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(191) DEFAULT 'k0kubun',
-    created_at DATETIME NOT NULL,
+    `id`       bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `name`     varchar(191) DEFAULT 'k0kubun',
+    created_at DATETIME            NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-ALTER TABLE user ADD INDEX index_name(name);
+ALTER TABLE user
+    ADD INDEX index_name (name);
+
+CREATE TABLE posts
+(
+    id    int         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(50) NOT NULL DEFAULT ''
+);
