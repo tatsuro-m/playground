84327b7631678e74cdc8d06a3aca04f7bbdec2b0
tatsuro-m <chrome0130@gmail.com>
2021-10-19 12:44:05 +0900

    add initial ddl

---
+++ sql/sqldef/mysqldef/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user`
+(
+    `id`   bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(191) DEFAULT 'k0kubun',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
