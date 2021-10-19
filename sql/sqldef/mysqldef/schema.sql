3e516c83bdf46669eb55d1fa37e2b979d4460b86
tatsuro-m <chrome0130@gmail.com>
2021-10-19 12:45:08 +0900

    update schema

--- sql/sqldef/mysqldef/schema.sql
+++ sql/sqldef/mysqldef/schema.sql
@@ -2,6 +2,9 @@ CREATE TABLE `user`
 (
     `id`   bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(191) DEFAULT 'k0kubun',
+    created_at DATETIME NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE user ADD INDEX index_name(name);
