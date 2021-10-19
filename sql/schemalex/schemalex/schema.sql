48377f3802094f00c831bc263464f92f40e56b9f
tatsuro-m <chrome0130@gmail.com>
2021-10-19 17:02:31 +0900

    update schema

--- sql/schemalex/schemalex/schema.sql
+++ sql/schemalex/schemalex/schema.sql
@@ -1,5 +1,11 @@
 CREATE TABLE hoge
 (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id)
+    id    INTEGER     NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    title varchar(50) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE posts
+(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
