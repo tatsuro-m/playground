ee182164558bc68cf1c4db7d34073d51b8d0d755
tatsuro-m <chrome0130@gmail.com>
2021-11-05 14:41:30 +0900

    imple

--- go/yaml/main.go
+++ go/yaml/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	b, err := ioutil.ReadFile("./kustomization.yaml")
+	kFilePath := "./kustomization.yaml"
+	b, err := ioutil.ReadFile(kFilePath)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -20,7 +22,32 @@ func main() {
 		return
 	}
 
-	for k, v := range m {
-		fmt.Printf("key: %s, value: %d\n", k, v)
+	secretFileName := "secret.yaml"
+	if val, ok := m["resources"]; ok {
+		s, ok := val.([]string)
+		if !ok {
+			fmt.Println("[]string にキャストできなかった")
+		}
+
+		s = append(s, secretFileName)
+		m["resources"] = s
+	} else {
+		m["resources"] = []string{secretFileName}
 	}
+
+	f, err := os.OpenFile(kFilePath, os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	data, err := yaml.Marshal(m)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(data))
+	f.Write(data)
 }
