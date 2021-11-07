7923fa6b02214cb68c4f6b297b32a120de4c2210
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-07 11:08:02 +0900

    add posts test

---
+++ graphql/gqlgen-sample/graphql/graph/resolver/post_resolver_test.go
@@ -0,0 +1,41 @@
+package resolver
+
+import (
+	"graphql/thelper"
+	"testing"
+
+	"github.com/sebdah/goldie/v2"
+)
+
+func TestQueryResolver_Posts(t *testing.T) {
+	c := createGqlClient(t)
+	g := goldie.New(t)
+
+	table := []struct {
+		name  string
+		query string
+	}{
+		{
+			name:  "id と title が返ってくること",
+			query: `query posts {  posts {  id  title  }}`,
+		},
+		{
+			name:  "user も含めて返ってくること",
+			query: `query users {  posts {  id title user { name email }  }}`,
+		},
+	}
+
+	for _, td := range table {
+		t.Run(td.name, func(t *testing.T) {
+			thelper.SetupTest(t)
+			defer thelper.FinalizeTest(t)
+
+			u := thelper.InsertUser(t, 1)[0]
+			thelper.InsertPost(t, 5, u.ID)
+
+			var resp interface{}
+			c.MustPost(td.query, &resp)
+			g.AssertJson(t, t.Name(), resp)
+		})
+	}
+}
