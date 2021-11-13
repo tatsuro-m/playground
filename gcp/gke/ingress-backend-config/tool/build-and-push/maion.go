b96a7f5f9ae0525198956aff2d5e99a1f7c44d8a
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-13 14:41:37 +0900

    update script

--- gcp/gke/ingress-backend-config/tool/build-and-push/maion.go
+++ gcp/gke/ingress-backend-config/tool/build-and-push/maion.go
@@ -9,16 +9,20 @@ import (
 )
 
 func main() {
-	fmt.Println("start")
+	fmt.Println("build and push start")
 	dockerBuildAndPush()
 }
 
 func dockerBuildAndPush() {
 	for _, service := range lib.Services {
-		imageTag := fmt.Sprintf("%s/%s/%s-%s-%s:%d", lib.Host, lib.ProjectID, lib.Env, lib.AppName, service, time.Now().Unix())
+		imageTag := fmt.Sprintf("%s/%s/%s-%s-%s/%s:%d", lib.Host, lib.ProjectID, lib.Env, lib.AppName, service, service, time.Now().Unix())
+
 		cmd := splitCmd(fmt.Sprintf("docker build -f ./%s/Dockerfile --platform amd64 -t %s ./%s", service, imageTag, service))
-		out, err := exec.Command(cmd[0], cmd[1:]...).Output()
-		fmt.Println(err)
+		out, _ := exec.Command(cmd[0], cmd[1:]...).Output()
+		fmt.Println(string(out))
+
+		cmd = splitCmd(fmt.Sprintf("docker push %s", imageTag))
+		out, _ = exec.Command(cmd[0], cmd[1:]...).Output()
 		fmt.Println(string(out))
 	}
 }
