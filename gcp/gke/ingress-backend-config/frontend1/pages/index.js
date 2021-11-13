58a82f05b8f33c9e5adf8c82bf938a77ce637dbb
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-13 16:33:09 +0900

    display free image

--- gcp/gke/ingress-backend-config/frontend1/pages/index.js
+++ gcp/gke/ingress-backend-config/frontend1/pages/index.js
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1>This is frontend1!!!!</h1>
+    <div>
+      <h1>This is frontend1!!!!</h1>
+      <img
+        src="https://images.unsplash.com/photo-1636743716007-d45b27bebead?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=770&q=80"
+        alt="cat"/>
+      <img
+        src="https://images.unsplash.com/photo-1636493532482-44ec2354476b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        alt="cat"/>
+    </div>
   )
 }
