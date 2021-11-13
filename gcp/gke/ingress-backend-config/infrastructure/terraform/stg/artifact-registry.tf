478c810a243431673edd5678ba9ba0121057b050
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-13 14:30:01 +0900

    modify registry id

--- gcp/gke/ingress-backend-config/infrastructure/terraform/stg/artifact-registry.tf
+++ gcp/gke/ingress-backend-config/infrastructure/terraform/stg/artifact-registry.tf
@@ -8,22 +8,12 @@ resource "google_artifact_registry_repository" "nginx" {
   format        = "DOCKER"
 }
 
-resource "google_artifact_registry_repository" "front1" {
+resource "google_artifact_registry_repository" "frontend1" {
   provider = google-beta
   project  = "playground-318023"
 
   location      = var.default_region
-  repository_id = "${local.app_prefix}-front1"
-  description   = "web front end 1"
-  format        = "DOCKER"
-}
-
-resource "google_artifact_registry_repository" "front2" {
-  provider = google-beta
-  project  = "playground-318023"
-
-  location      = var.default_region
-  repository_id = "${local.app_prefix}-front2"
-  description   = "web front end 2"
+  repository_id = "${local.app_prefix}-frontend1"
+  description   = "web frontend 1"
   format        = "DOCKER"
 }
