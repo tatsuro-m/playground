e31d6c2b7a9d67cbff54b07e42f8628e4e8314fb
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-13 16:56:04 +0900

    add image

--- gcp/gke/ingress-backend-config/frontend1/pages/index.js
+++ gcp/gke/ingress-backend-config/frontend1/pages/index.js
@@ -3,6 +3,10 @@ export default function Home() {
     <div>
       <h1>This is frontend1!!!!</h1>
       <img
+        src="images/local.jpeg"
+        alt="local"
+      />
+      <img
         src="https://images.unsplash.com/photo-1636743716007-d45b27bebead?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=770&q=80"
         alt="cat"/>
       <img
