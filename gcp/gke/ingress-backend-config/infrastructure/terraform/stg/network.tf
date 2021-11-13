7e109234901a0256c937a2d7260cba912435f129
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-13 15:45:57 +0900

    create static ip

--- gcp/gke/ingress-backend-config/infrastructure/terraform/stg/network.tf
+++ gcp/gke/ingress-backend-config/infrastructure/terraform/stg/network.tf
@@ -8,3 +8,7 @@ resource "google_compute_subnetwork" "main" {
   ip_cidr_range = "10.2.0.0/16"
   network       = google_compute_network.main_vpc.id
 }
+
+resource "google_compute_global_address" "default" {
+  name = "ingress-test"
+}
