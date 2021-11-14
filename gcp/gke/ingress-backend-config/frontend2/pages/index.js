e47661695d28d051469e7fc3fc8b1da3d0994f3f
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-14 10:15:30 +0900

    add frontend2

---
+++ gcp/gke/ingress-backend-config/frontend2/pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>This is frontend2!!!!</h1>
+      <img
+        src="images/local.jpeg"
+        alt="local"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1636743716007-d45b27bebead?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=770&q=80"
+        alt="cat"/>
+      <img
+        src="https://images.unsplash.com/photo-1636493532482-44ec2354476b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        alt="cat"/>
+    </div>
+  )
+}
