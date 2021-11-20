a2b71b7a83c84e3ae69b4f170a696232222b752d
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-20 11:30:30 +0900

    delete artifact registry role

--- gcp/cloud-run/next-deploy/tool/build-and-push/main.go
+++ gcp/cloud-run/next-deploy/tool/build-and-push/main.go
@@ -5,21 +5,22 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	fmt.Println("build and push start")
 
-	// コミットSHA とかを動的に取っても良いけど今回は簡単に latest でやる
-	tag := "latest"
+	// コミットSHA とかを動的に取っても良いけど今回は簡単に unix 時でタグ付けする。
+	tag := time.Now().Unix()
 	fmt.Println(tag)
 	for _, service := range lib.Services {
 		dockerBuildAndPush(service, tag)
 	}
 }
 
-func dockerBuildAndPush(service string, tag string) {
-	imageTag := fmt.Sprintf("%s/%s/%s-%s-%s/%s:%s", lib.Host, lib.ProjectID, lib.Env, lib.AppName, service, service, tag)
+func dockerBuildAndPush(service string, tag int64) {
+	imageTag := fmt.Sprintf("%s/%s/%s-%s-%s/%s:%d", lib.Host, lib.ProjectID, lib.Env, lib.AppName, service, service, tag)
 
 	cmd := splitCmd(fmt.Sprintf("docker build -f ./%s/Dockerfile --platform amd64 -t %s ./%s", service, imageTag, service))
 	out, _ := exec.Command(cmd[0], cmd[1:]...).Output()
