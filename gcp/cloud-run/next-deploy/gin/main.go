2d602b52dc2993e61b12c665d248612ff73e47e3
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-20 12:22:15 +0900

    setup gin api

---
+++ gcp/cloud-run/next-deploy/gin/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":" + os.Getenv("PORT"))
+}
