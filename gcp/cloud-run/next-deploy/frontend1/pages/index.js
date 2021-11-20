e4f5a20fa39791b7c667276214c0c510329e4851
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-20 11:33:24 +0900

    update content

--- gcp/cloud-run/next-deploy/frontend1/pages/index.js
+++ gcp/cloud-run/next-deploy/frontend1/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          Welcome to GCP Cloud Run!
         </h1>
 
         <p className={styles.description}>
