5ba0bf41c7392494a59651f45ee9b2ee2fd3b6c4
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-21 15:03:39 +0900

    update content

--- gcp/cloud-run/next-deploy/frontend1/pages/index.js
+++ gcp/cloud-run/next-deploy/frontend1/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello Cloud Run!!
+          Hello CIOps!!
         </h1>
 
         <p className={styles.description}>
