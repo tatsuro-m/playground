2e9f805a87d3403892cf6f2439b90d61e05f8662
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-27 11:41:56 +0900

    add meth

--- graphql/gqlgen-sample/graphql/service/tag/tag.go
+++ graphql/gqlgen-sample/graphql/service/tag/tag.go
@@ -2,12 +2,30 @@ package tag
 
 import (
 	"context"
+	"fmt"
 	"graphql/db"
 	"graphql/models"
+
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 type Service struct{}
 
 func (s Service) GetByID(id int) (*models.Tag, error) {
 	return models.FindTag(context.Background(), db.GetDB(), id)
+}
+
+func (s Service) Posts(tagID int) (models.PostSlice, error) {
+	posts, err := models.Posts(
+		qm.InnerJoin("post_tags pt on id = pt.post_id"),
+		qm.InnerJoin("tags t on pt.tag_id = t.id"),
+		qm.Where("t.id = ?", tagID),
+	).All(context.Background(), db.GetDB())
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return posts, nil
 }
