5d5949e9b453def9bf756c4161cb9f610b6da4ad
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-11-29 07:50:51 +0900

    fmt

--- graphql/gqlgen-sample/frontend/pages/sign_in.tsx
+++ graphql/gqlgen-sample/frontend/pages/sign_in.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import {auth, firebaseUser, uiConfig} from '../src/lib/firebase'
+import { auth, firebaseUser, uiConfig } from '../src/lib/firebase'
 import { useRouter } from 'next/router'
 
 const sign_in: React.VFC = () => {
