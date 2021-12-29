ff1cfc644a96896374266745f3a2bb4c02fcc9cb
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-12-29 22:57:40 +0900

    add tables

---
+++ graphql/postal-code/migrations/000003_create_prefectures_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS prefectures
+(
+    id         int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name       varchar(50)        NOT NULL,
+    name_roma varchar(50)        NOT NULL,
+    created_at DATETIME           NOT NULL DEFAULT now(),
+    updated_at DATETIME           NOT NULL DEFAULT now()
+);
