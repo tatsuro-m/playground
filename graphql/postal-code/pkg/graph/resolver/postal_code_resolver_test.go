837d2c208e205821ab63eaf6595b066cf24c79d3
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-12-31 11:19:55 +0900

    add test case

--- graphql/postal-code/pkg/graph/resolver/postal_code_resolver_test.go
+++ graphql/postal-code/pkg/graph/resolver/postal_code_resolver_test.go
@@ -51,4 +51,31 @@ func TestQueryResolver_PostalCode(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("異常系", func(t *testing.T) {
+		table := []struct {
+			name    string
+			address string
+			query   string
+		}{
+			{
+				name:    "途中に半角スペースを入れるとエラーが返ってくること",
+				address: "北海道札幌市 中央区旭ケ丘",
+				query:   "query address($address: String!) {\n  postalCode(address: $address) {\n    id\n    code\n}\n}",
+			},
+		}
+
+		for _, td := range table {
+			t.Run(td.name, func(t *testing.T) {
+				thelper.SetupTest(t)
+				defer thelper.FinalizeTest(t)
+				thelper.InsertAddressData(t)
+
+				var resp interface{}
+				err := c.Post(td.query, &resp, client.Var("address", td.address))
+				g.AssertJson(t, t.Name(), resp)
+				g.AssertJson(t, t.Name()+"_error", err)
+			})
+		}
+	})
 }
