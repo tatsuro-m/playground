bb451c69512b829c6f31422e614f0372b9dc6264
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-12-31 10:36:35 +0900

    全角スペース付きで送られても返せるように修正

--- graphql/postal-code/pkg/service/postalcode/postalcode.go
+++ graphql/postal-code/pkg/service/postalcode/postalcode.go
@@ -5,6 +5,7 @@ import (
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"pcode/pkg/db"
 	"pcode/pkg/models"
+	"pcode/pkg/util"
 )
 
 type Service struct{}
@@ -14,7 +15,7 @@ func (s Service) GetOne(address string) (*models.PostalCode, error) {
 		qm.LeftOuterJoin("prefectures p on p.id = postal_codes.prefecture_id"),
 		qm.LeftOuterJoin("municipalities m on m.id = postal_codes.municipality_id"),
 		qm.LeftOuterJoin("town_areas t on t.id = postal_codes.town_area_id"),
-		qm.Where("CONCAT(p.name,m.name,t.name) = ?", address),
+		qm.Where("CONCAT(p.name,m.name,t.name) = ?", util.TrimFullWidthSpace(address)),
 	).One(context.Background(), db.GetDB())
 
 	if err != nil {
