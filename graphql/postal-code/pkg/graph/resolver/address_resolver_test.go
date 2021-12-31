96419386fa9b126e9728ddf17820c10546d00a9e
Tatsuro Miyamoto <chrome0130@gmail.com>
2021-12-31 09:36:34 +0900

    add test case

--- graphql/postal-code/pkg/graph/resolver/address_resolver_test.go
+++ graphql/postal-code/pkg/graph/resolver/address_resolver_test.go
@@ -1,6 +1,7 @@
 package resolver
 
 import (
+	"fmt"
 	"github.com/99designs/gqlgen/client"
 	"github.com/sebdah/goldie/v2"
 	"pcode/pkg/thelper"
@@ -38,6 +39,49 @@ func TestQueryResolver_Address(t *testing.T) {
 				var resp interface{}
 				c.MustPost(td.query, &resp, client.Var("postal_code", td.postalCode))
 				g.AssertJson(t, t.Name(), resp)
+			})
+		}
+	})
+
+	t.Run("異常系", func(t *testing.T) {
+		table := []struct {
+			name       string
+			postalCode string
+			query      string
+		}{
+			{
+				name:       "１0桁を指定するとエラーになること",
+				postalCode: "0000000000",
+				query:      "query address($postal_code: String!) {\n  address(postal_code: $postal_code) {\n    id\n    name\n}\n}",
+			},
+			{
+				name:       "存在しない郵便番号を指定するとエラーになること",
+				postalCode: "1111111",
+				query:      "query address($postal_code: String!) {\n  address(postal_code: $postal_code) {\n    id\n    name\n}\n}",
+			},
+			{
+				name:       "アルファベットを指定するとエラーになること",
+				postalCode: "aaaaaaa",
+				query:      "query address($postal_code: String!) {\n  address(postal_code: $postal_code) {\n    id\n    name\n}\n}",
+			},
+			{
+				name:       "ひらがなを指定するとエラーになること",
+				postalCode: "あああああああ",
+				query:      "query address($postal_code: String!) {\n  address(postal_code: $postal_code) {\n    id\n    name\n}\n}",
+			},
+		}
+
+		for _, td := range table {
+			t.Run(td.name, func(t *testing.T) {
+				thelper.SetupTest(t)
+				defer thelper.FinalizeTest(t)
+				thelper.InsertAddressData(t)
+
+				var resp interface{}
+				err := c.Post(td.query, &resp, client.Var("postal_code", td.postalCode))
+				fmt.Println(t.Name())
+				g.AssertJson(t, t.Name(), resp)
+				g.AssertJson(t, t.Name()+"_error", err)
 			})
 		}
 	})
