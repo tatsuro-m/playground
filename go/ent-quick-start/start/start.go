5df960ff6bfe9f3446b4525ba2b909a11c7082dd
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-01-03 11:46:08 +0900

    uid を主キーにするように変更

--- go/ent-quick-start/start/start.go
+++ go/ent-quick-start/start/start.go
@@ -24,8 +24,12 @@ func main() {
 
 	ctx := context.Background()
 	client.Schema.WriteTo(ctx, os.Stdout, migrate.WithDropColumn(true), migrate.WithDropIndex(true))
-	// オートマイグレーションツールを実行する
-	if err := client.Schema.Create(ctx, migrate.WithDropIndex(true), migrate.WithDropColumn(true)); err != nil {
+
+	err = client.Schema.Create(
+		ctx, migrate.WithDropIndex(true),
+		migrate.WithDropColumn(true),
+		migrate.WithGlobalUniqueID(true))
+	if err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
