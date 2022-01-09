ef36354a87589b5d9c1fdc389a19dc39d4789e46
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-01-09 09:26:43 +0900

    add comment

--- graphql/gqlgen-sample/graphql/migrations/migration.sh
+++ graphql/gqlgen-sample/graphql/migrations/migration.sh
@@ -3,3 +3,7 @@
 # docker compose migration サービスの起動用スクリプト
 migrate -database mysql://root:password@tcp\(db:3306\)/dev -path migrations/ up && \
  migrate -database mysql://root:password@tcp\(db:3306\)/test -path migrations/ up
+
+## down バージョン
+#migrate -database mysql://root:password@tcp\(db:3306\)/dev -path migrations/ down && \
+# migrate -database mysql://root:password@tcp\(db:3306\)/test -path migrations/ down
