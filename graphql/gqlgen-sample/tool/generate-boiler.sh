37abec971c1b709c138e6d9d62648ed9cf7de483
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-01-09 09:10:30 +0900

    remove script

--- graphql/gqlgen-sample/tool/generate-boiler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-echo 'sqlboiler の generator を起動します'
-docker compose run --rm boiler
--- graphql/gqlgen-sample/tool/generate-gqlgen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-echo 'gqlgen の generator を起動します'
-docker compose run --rm gqlgen_generator
