84fddc1c7e04976f9296fc383e4ca1c3c875cb6e
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-01-09 10:28:29 +0900

    config

---
+++ go/sa-generator/generator/internal/sagenerator/config.go
@@ -0,0 +1,15 @@
+package sagenerator
+
+type Config struct {
+	TargetEnvs     []string
+	TargetServices []string
+}
+
+func getConfig() *Config {
+	c := &Config{
+		TargetEnvs:     []string{"dev", "stg", "preprod", "prod"},
+		TargetServices: []string{"a", "b", "c", "d", "e"},
+	}
+
+	return c
+}
