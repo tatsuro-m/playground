3c09eb694af618bc09548d8665fa970180a51d58
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-01-09 10:41:52 +0900

    base test

---
+++ go/sa-generator/generator/internal/sagenerator/models/sa_test.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"generator/internal/sagenerator/config"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetMatrix(t *testing.T) {
+	tb := []struct {
+		name string
+		in   *config.Config
+		out  *[]Sa
+	}{
+		{
+			name: "dev と stg で正常に返ってくること",
+			in:   &config.Config{TargetEnvs: []string{"dev", "stg"}, TargetServices: []string{"a", "b"}},
+			out:  &[]Sa{{Env: "dev", ServiceName: "a"}, {Env: "dev", ServiceName: "b"}, {Env: "stg", ServiceName: "a"}, {Env: "stg", ServiceName: "b"}},
+		},
+	}
+
+	for _, tt := range tb {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := GetMatrix(tt.in)
+			assert.Equal(t, tt.out, actual)
+		})
+	}
+}
