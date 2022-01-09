438bb16817f1161f400e8a1b7ed5cbec16142188
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-01-09 09:25:09 +0900

    シェルスクリプトを起動するように修正

---
+++ graphql/gqlgen-sample/graphql/migrations/migration.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# docker compose migration サービスの起動用スクリプト
+migrate -database mysql://root:password@tcp\(db:3306\)/dev -path migrations/ up && \
+ migrate -database mysql://root:password@tcp\(db:3306\)/test -path migrations/ up
