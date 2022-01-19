32fa0d55e65fe301778e86c4f8364f886801ce68
tatsuro-m <chrome0130@gmail.com>
2022-01-19 15:43:01 +0900

    for

--- python/getting_started/loop.py
+++ python/getting_started/loop.py
@@ -3,3 +3,16 @@ num = 1
 while num < 10:
     print(num)
     num = num + 1
+
+print('------------------------------')
+
+# for
+arr = [1, 2, 3, 4, 5]
+for num in arr:
+    print(num)
+
+# while でも len を使えば同じことができる。
+i = 0
+while i < len(arr):
+    print(arr[i])
+    i = i + 1
