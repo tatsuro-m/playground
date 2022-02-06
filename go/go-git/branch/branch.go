2dbe8ad178b2acd5e8debcc6e18cbba58fbbf892
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-02-06 10:18:12 +0900

    branch

---
+++ go/go-git/branch/branch.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"log"
+)
+
+func main() {
+	repo := git.Repository{Storer: memory.NewStorage()}
+	branches, err := repo.Branches()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(branches)
+
+	tree, err := repo.Worktree()
+	log.Print(tree)
+}
