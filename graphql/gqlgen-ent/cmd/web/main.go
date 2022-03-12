d8aca37a45c65a08895446c3bed6f77abace4c34
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-03-13 08:51:09 +0900

    Revert "wip: pkg デバッグ中"
    This reverts commit 1b4baf3690b3db1ae41d76b2ccd3eaeb173089a7.

--- graphql/gqlgen-ent/cmd/web/main.go
+++ graphql/gqlgen-ent/cmd/web/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"gqlgent/internal/web/graph/generated"
-	"gqlgent/internal/web/graph/resolver"
+	"gqlgent/graph/generated"
+	"gqlgent/graph/resolver"
 	"log"
 	"net/http"
 	"os"
