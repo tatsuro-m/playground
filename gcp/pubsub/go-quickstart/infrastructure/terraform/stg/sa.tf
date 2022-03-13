633ff6a4ac62d7e5b56c36f6d2e86d2e63b0c40a
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-03-13 09:33:56 +0900

    sa key

--- gcp/pubsub/go-quickstart/infrastructure/terraform/stg/sa.tf
+++ gcp/pubsub/go-quickstart/infrastructure/terraform/stg/sa.tf
@@ -2,3 +2,14 @@ resource "google_service_account" "main_topic" {
   account_id   = "${local.app_prefix}-main-topic"
   display_name = "pubsub main topic sa"
 }
+
+resource "google_service_account_key" "main_topic_key" {
+  service_account_id = google_service_account.main_topic.name
+}
+
+resource "local_file" main_topic_key {
+  filename             = "./output/secrets/${google_service_account.main_topic.name}.json"
+  content              = base64decode(google_service_account_key.main_topic_key.private_key)
+  file_permission      = "0600"
+  directory_permission = "0755"
+}
