200442f3388b1fde9300ff18799f78c4cac5bfa2
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-05-23 13:41:21 +0900

    Docker 入れるところまで

--- gcp/gcs/gcsfuse/terraform/gce.tf
+++ gcp/gcs/gcsfuse/terraform/gce.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance_template" "tpl" {
   ]
 
   disk {
-    source_image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
     auto_delete  = true
     disk_size_gb = 30
     boot         = true
--- gcp/gcs/gcsfuse/terraform/script/start.sh
+++ gcp/gcs/gcsfuse/terraform/script/start.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# gcs fuse の設定
-sudo tee /etc/yum.repos.d/gcsfuse.repo > /dev/null <<EOF
-[gcsfuse]
-name=gcsfuse (packages.cloud.google.com)
-baseurl=https://packages.cloud.google.com/yum/repos/gcsfuse-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=0
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
+# gcs fuse の設定（https://github.com/GoogleCloudPlatform/gcsfuse/blob/master/docs/installing.md）
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo yum install gcsfuse -y
+sudo apt-get update
+sudo apt-get install -y gcsfuse
 
 mkdir -p /root/gcsfuse/1
 gcsfuse stg-gcsfuse-test1 /root/gcsfuse/1
 
 mkdir -p /root/gcsfuse/2
 gcsfuse stg-gcsfuse-test2 /root/gcsfuse/2
+
+# Docker のインストール（https://docs.docker.com/engine/install/ubuntu/）
+sudo apt-get update
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
