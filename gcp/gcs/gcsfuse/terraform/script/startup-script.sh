5b3d138cfb6fefe08004db2a20beefb0baba0aa1
Tatsuro Miyamoto <chrome0130@gmail.com>
2022-05-24 11:02:30 +0900

    disk mount

--- gcp/gcs/gcsfuse/terraform/script/startup-script.sh
+++ gcp/gcs/gcsfuse/terraform/script/startup-script.sh
@@ -3,6 +3,7 @@
 # メタデータサーバーから、初回のみ必要なスクリプトを書き込む。
 curl -X GET --location "http://metadata.google.internal/computeMetadata/v1/instance/attributes/init-script" -H "Metadata-Flavor: Google" > /root/init.sh
 
+sudo mount -o discard,defaults /dev/sdb /usr/share/elasticsearch/data
 sudo -u elasticsearch bash -c '
 gcsfuse stg-gcsfuse-test1 /usr/share/elasticsearch/config/user_dictionary
 /usr/share/elasticsearch/bin/elasticsearch
