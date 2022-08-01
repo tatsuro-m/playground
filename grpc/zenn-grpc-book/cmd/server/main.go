6a82cd61ccb3b331d290aa778f44c4431fb0718d
tatsuro-m <chrome0130@gmail.com>
2022-08-01 15:17:05 +0900

    imple

---
+++ grpc/zenn-grpc-book/cmd/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	hellopb "mygrpc/pkg/grpc"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+
+	// ここで明示的に Service を登録する
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{Message: fmt.Sprintf("Hello %s!", req.GetName())}, nil
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
