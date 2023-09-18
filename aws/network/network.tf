c0194215fb606adf02966d3aa31f6c31a79b6f8c
Tatsuro Miyamoto <chrome0130@gmail.com>
2023-09-18 14:26:06 +0900

    Revert "テスト目的で一時的に public サブネットを private にする"
    This reverts commit ce7646aa1f1a2fe4b864250c7733af99067f4a00.

--- aws/network/network.tf
+++ aws/network/network.tf
@@ -70,7 +70,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_c" {
