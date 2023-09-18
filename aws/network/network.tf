8e3c125705552a6a313a70e15646126bbda5467d
Tatsuro Miyamoto <chrome0130@gmail.com>
2023-09-18 11:54:25 +0900

    add vpc

---
+++ aws/network/network.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "Public subnet -a"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.11.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "Private subnet -a"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "Public subnet -c"
+  }
+}
+
+resource "aws_subnet" "private_c" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.12.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "Private subnet -c"
+  }
+}
