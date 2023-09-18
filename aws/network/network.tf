c1660bdb8182f1c7bb3ff14636eba6672065569b
Tatsuro Miyamoto <chrome0130@gmail.com>
2023-09-18 15:17:47 +0900

    rm route

--- aws/network/network.tf
+++ aws/network/network.tf
@@ -81,10 +81,13 @@ resource "aws_route_table_association" "public_c" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
+#  VPC Endpoint を使って VPC 外部のAWSサービスに接続するようにするので、NAT Gateway との接続は切っておく（Internet Gateway 経由での通信ではなくす）
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    nat_gateway_id = aws_nat_gateway.main.id
+#  }
+
+  route = []
 
   tags = {
     Name = "Private Route Table"
