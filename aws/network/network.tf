24bf7bff63d271a28e263d35f9295281841c66d3
Tatsuro Miyamoto <chrome0130@gmail.com>
2023-09-18 11:57:01 +0900

    igw

--- aws/network/network.tf
+++ aws/network/network.tf
@@ -7,6 +7,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "handson-igw"
+  }
+}
+
 resource "aws_subnet" "public_a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
